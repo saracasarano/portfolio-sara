f307444d6e42d4fbf03b9729588a2af3787e66f6
Sara Casarano <sara.casarano@edu.unito.it>
2025-09-25 01:46:25 +0200

    modifica bottone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
           Creo esperienze visive uniche attraverso un design moderno e
           funzionale.
         </p>
-        <router-link to="/progetti" class="btn btn-bordeaux mt-3">
+        <router-link to="/progetti" class="btn btn-bordeaux btn-large mt-3">
           Let's get started →
         </router-link>
       </div>
